1e4f0971f8faddeb1315648ed1198d2c535f6b3e
Dominic Barnes <dominic@dbarnes.info>
2015-10-23 21:28:22 -0700

    export properties with hyphens for better compatibility downstream

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var rels = require('./meta.json');
 var camel = require('to-camel-case');
 
 rels.forEach(function (rel) {
-  exports[camel(rel.name)] = rel;
+  exports[camel(rel.name)] = exports[rel.name] = rel;
 });
